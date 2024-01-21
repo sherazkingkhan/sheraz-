076144515d141a893c24a85f5f3c806f8f767d3b
sherazkingkhan <156561931+sherazkingkhan@users.noreply.github.com>
2024-01-21 10:44:07 +0500

    Add files via upload

---
+++ sheraz_khan_midterm.py
@@ -0,0 +1,18 @@
+s= float(input("Enter the  fist nuber of addeitoin "))
+f=floatinput(nput("Enter tha secand nuber for addeition "))
+g=s=fist
+prnit("sum:"g)
+qw= floatinput('input("Enter tha divdend for division')")
+Er=float
+"input("Enter tha divisor for division"))
+gh=qw/Er
+print("division:gh")
+a= int(input("Entr nuber"))
+b=int(input("Enter nuber")
+c=a **b
+x=int(input)("Enter divident")
+y=int(input)"Enter divident")
+c=x//y
+#Q4
+a=("10")
+print(type)
