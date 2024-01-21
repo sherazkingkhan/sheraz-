c59dac595b69dee11b1f544156b4d56518e88b7d
sherazkingkhan <156561931+sherazkingkhan@users.noreply.github.com>
2024-01-21 10:49:22 +0500

    Update sheraz_khan_midterm.py

--- sheraz_khan_midterm.py
+++ sheraz_khan_midterm.py
@@ -1,18 +1,28 @@
+# Question 1
 s= float(input("Enter the  fist nuber of addeitoin "))
-f=floatinput(nput("Enter tha secand nuber for addeition "))
-g=s=fist
+f=float(input("Enter tha secand nuber for addeition "))
+
+g=s+f
 prnit("sum:"g)
-qw= floatinput('input("Enter tha divdend for division')")
-Er=float
-"input("Enter tha divisor for division"))
+
+qw= float(input("Enter tha divdend for division"))
+Er=float(input("Enter tha divisor for division"))
 gh=qw/Er
-print("division:gh")
+print("division:gh" , gh)
+
 a= int(input("Entr nuber"))
 b=int(input("Enter nuber")
-c=a **b
-x=int(input)("Enter divident")
-y=int(input)"Enter divident")
+c=a**b
+
+print(c)
+
+
+x=int(input("Enter divident"))
+y=int(input("Enter divident"))
 c=x//y
+
+print(c)
+
 #Q4
 a=("10")
 print(type)
